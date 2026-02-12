34b1231b0f5550111c784d8546f9dd6513b98a11
livchea-dev <livchea.chea@suntel.io>
2026-02-12 16:35:40 +0700

    add status for usersubsription

--- app/Enums/UserSubscriptionStatus.php
+++ app/Enums/UserSubscriptionStatus.php
@@ -7,4 +7,6 @@ enum UserSubscriptionStatus: string
     case ACTIVE = 'active';
     case EXPIRED = 'expired';
     case INACTIVE = 'inactive';
+    case PENDING_PAYMENT = 'pending_payment';
 }
+    
